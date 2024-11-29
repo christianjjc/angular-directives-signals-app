28d341a5ce5de64ffe27a236e9d5a8d9dd41f942
Christian John jiménez Calvo <christianjjc@gmail.com>
2024-11-29 16:20:58 -0500

    seccion-24-ep-11-mutaciones-actualizar-parte-del-estado-objeto-valor

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
   standalone: false,
-  
+
   templateUrl: './properties-page.component.html',
-  styleUrl: './properties-page.component.css'
+  styleUrl: './properties-page.component.css',
 })
 export class PropertiesPageComponent {
+  public user = signal<User>({
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  });
 
+  public fullName = computed(
+    () => `${this.user().first_name} ${this.user().last_name}`
+  );
+
+  onFieldUpdated(field: keyof User, value: string) {
+    //* Primera Forma
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value,
+    // });
+
+    //* angular 16+
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+      return { ...current };
+    });
+  }
 }
